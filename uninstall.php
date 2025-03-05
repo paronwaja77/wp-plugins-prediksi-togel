3eca10bd1b1b6df581ba6c4feb9c15b26f70f404
Paron Waja Group <paronwaja77@gmail.com>
2025-03-06 06:32:40 +0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,5 +3,5 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
-require_once plugin_dir_path(__FILE__) . 'includes/class-ptw-database.php';
-PTW_Database::delete_table();
+// require_once plugin_dir_path(__FILE__) . 'includes/class-ptw-database.php';
+// PTW_Database::delete_table();
